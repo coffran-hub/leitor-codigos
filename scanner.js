f6103ce6b83c5fe11ab648a03e3ad93128f4ba13
coffran-hub <coffranmarcelosantos@gmail.com>
2026-06-11 19:32:16 -0300

    Create scanner.js

---
+++ scanner.js
@@ -0,0 +1,52 @@
+const codeReader = new ZXing.BrowserMultiFormatReader();
+
+const video = document.getElementById('video');
+const result = document.getElementById('result');
+
+let currentStream = null;
+
+async function startScanner(){
+
+const devices =
+await codeReader.listVideoInputDevices();
+
+const selectedDevice =
+devices[0].deviceId;
+
+codeReader.decodeFromVideoDevice(
+selectedDevice,
+video,
+(resultScan, err)=>{
+
+if(resultScan){
+
+result.value = resultScan.text;
+
+saveHistory(resultScan.text);
+
+sendToAppsScript(resultScan.text);
+
+playBeep();
+
+}
+
+});
+
+}
+
+function stopScanner(){
+codeReader.reset();
+}
+
+async function scanImage(file){
+
+const img = URL.createObjectURL(file);
+
+const res =
+await codeReader.decodeFromImageUrl(img);
+
+result.value = res.text;
+
+saveHistory(res.text);
+
+}
